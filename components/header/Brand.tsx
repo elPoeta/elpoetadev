0dd675cdd6acaff973195082b035c2f197854188
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-09-03 14:45:59 -0300

    add theme provider

---
+++ components/header/Brand.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from './Logo'
+
+const Brand = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  )
+}
+
+export default Brand
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,6 @@ import Head from 'next/head'
 const Home: NextPage = () => {
   return (
     <div className=''>
-      <Head>
-        <title>elPoetaDev</title>
-        <meta name="description" content="Leonardo Tosetto" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className=''>
         <h2>El Poeta Dev</h2>
       </main>
