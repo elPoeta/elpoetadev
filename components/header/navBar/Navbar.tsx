3dbaf2bd7edcb7ac667fb11e79d60e2ec6ded581
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-09-08 09:06:45 -0300

    add display navbar desktop version

--- components/header/navBar/Navbar.tsx
+++ components/header/navBar/Navbar.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { MenuButton } from './MenuButton'
+import { MenuItem } from './MenuItem'
 import ThemeButton from './ThemeButton'
 
 const Navbar = () => {
   return (
-    <nav>
-      <ThemeButton classNames='hidden lg:flex lg:w-6 lg:h-6 lg:cursor-pointer' />
+    <nav className='lg:flex items-center justify-center'>
+      <ul className='hidden lg:flex lg:flex-row'>
+        <MenuItem classNames="lg:text-xl lg:px-2 lg:font-bold lg:hover:text-[#0ea5e9]" />
+      </ul>
+      <ThemeButton classNames='hidden lg:flex lg:w-6 lg:h-6 lg:cursor-pointer lg:ml-3' />
       <MenuButton />
     </nav>
   )
