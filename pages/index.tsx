0a5771bef6e5a0ecd1ec6e62fc6704f7720b9a74
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-09-12 09:14:57 -0300

    add footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
+import { Footer } from '../components/footer/Footer'
 import { Hero } from '../components/home/Hero'
 
 const Home: NextPage = () => {
   return (
     <div className=''>
-      <main className='container overflow-x-hidden'>
+      <main className='overflow-x-hidden'>
         <Hero />
+        <Footer />
       </main>
     </div>
   )
